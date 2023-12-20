faa0eb84aaaee2063316d7354b06db8461437484
Jesse Warwick <jkeller647@gmail.com>
2023-12-19 19:04:50 -0800

    filled in the schema.sql with tables

---
+++ db/schema.sql
@@ -0,0 +1,23 @@
+-- Create department table
+CREATE TABLE IF NOT EXISTS department (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(30)
+);
+-- Create role table
+CREATE TABLE IF NOT EXISTS role (
+    id INTEGER PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
+-- Create employee table
+CREATE TABLE IF NOT EXISTS employee (
+    id INTEGER PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
